f35e2ec26d8571c6c0e08baf3fce5902ab8ad3fd
pandeypragati-tech <pragatipandey551@gmail.com>
2026-06-05 22:06:48 +0530

    Add Count digits , sum of digits , palindrome number programs

---
+++ Module01_Basics/P11_Sum_of_digits.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, sum = 0;
+
+    cout<< "Enter the number:";
+    cin >> n;
+
+    while(n > 0) {
+        sum += n % 10;
+        n /= 10;
+    }
+
+    cout << sum;
+
+    return 0;
+}
---
+++ Module01_Basics/P12_Palindrome_number.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout<< "Enter the number: ";
+    cin >> n;
+
+    int original = n;
+    int rev = 0;
+
+    while(n > 0) {
+        rev = rev * 10 + n % 10;
+        n /= 10;
+    }
+
+    if(original == rev)
+        cout << "Palindrome";
+    else
+        cout << "Not Palindrome";
+
+    return 0;
+}
